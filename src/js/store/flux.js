3245c04104be8becea9c91d71636b5a54272a9b6
Ivette Dominguez <ivettedominguez104@gmail.com>
2020-01-03 20:51:32 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,6 +6,21 @@ const getState = ({ getStore, setStore }) => {
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			newContact: (a, b, c, d) => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						full_name: a,
+						email: b,
+						phone: c,
+						address: d,
+						agenda_slug: "ivetted"
+					})
+				});
+
+				console.log("clicked", a, b, c, d);
+			}
 		}
 	};
 };
